0dd3a1869aef51db8a13877ef96b14990eedce7a
Anna Kioko <annakioko00@gmail.com>
2024-02-13 05:12:58 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { actors } from "../data";
 
-function Actors() {
-  return <div>{/*{code here}*/}</div>;
+const Actors = () => {
+  const actorItems = actors.map((actor) => (
+    <div key={actorItems.name}>
+      <h2>{actorItems.name}</h2>
+      <ul>
+        {actorItems.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorItems}
+    </div>
+  )
+  
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieItems = movies.map((movies) => (
+    <div key={movies.title}>
+      <h2>{movies.title}</h2>
+      <p>Runtime: {movies.time} min.</p>
+      <ul>
+        {movies.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieItems}
+    </div>
+  )
 }
 
 export default Movies;
